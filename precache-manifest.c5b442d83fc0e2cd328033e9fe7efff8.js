f34976635c819f03f0b9ff2b973eaebeee7804b4
Guaci Gutierrez <guacig@hotmail.com>
2020-10-25 13:04:15 +0000

    Updates

---
+++ precache-manifest.c5b442d83fc0e2cd328033e9fe7efff8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0bf1baf628358859d7bde877b78843",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "44b8bf577625c8b79af6",
+    "url": "/drum-machine/static/css/main.ced0386e.chunk.css"
+  },
+  {
+    "revision": "76e19ebf9108ca9a1171",
+    "url": "/drum-machine/static/js/2.60045754.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/drum-machine/static/js/2.60045754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44b8bf577625c8b79af6",
+    "url": "/drum-machine/static/js/main.8347fe0e.chunk.js"
+  },
+  {
+    "revision": "1d7cab7c1b093805e8ab",
+    "url": "/drum-machine/static/js/runtime-main.d679c66a.js"
+  }
+]);
